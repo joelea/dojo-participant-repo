3e7ec81302652f98a9e6edfef42dd583f7b99565
Joe Lea <jlea@palantir.com>
2014-08-27 13:20:59 +0100

    Clean up server code

--- extreme-startup-servers/java-jetty-bootstrap/src/main/java/ExtremeStartup.java
+++ extreme-startup-servers/java-jetty-bootstrap/src/main/java/ExtremeStartup.java
@@ -11,18 +11,24 @@ public class ExtremeStartup extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("A request has arrived");
-        resp.getWriter().write(answer(req.getParameter("q")));
+        String parameter = req.getParameter("q");
+
+        System.out.println("A request has arrived:");
+        System.out.println(parameter);
+
+        resp.getWriter().write(answer(parameter));
     }
 
     String answer(String parameter) {
         if (parameter == null)
             return "team name";
+
         Matcher additionMatcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(parameter);
         if (additionMatcher.matches()) {
             return String.valueOf(Integer.parseInt(additionMatcher.group(1))
                     + Integer.parseInt(additionMatcher.group(2)));
         }
+
         return "team name";
     }
 
