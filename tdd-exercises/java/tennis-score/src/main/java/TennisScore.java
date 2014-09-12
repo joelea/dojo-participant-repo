7b3d8f3f9f8be25146f26bd58b1405f24e2edbf8
Joe Lea <jlea@palantir.com>
2014-09-12 16:23:47 +0100

    Rename ITennisScore into TennisScore

--- tdd-exercises/java/tennis-score/src/main/java/TennisScore.java
+++ tdd-exercises/java/tennis-score/src/main/java/TennisScore.java
@@ -1,38 +1,7 @@
+public interface TennisScore {
+    SimpleTennisScore playerOneScores();
 
-public class TennisScore implements ITennisScore {
-    public int playerOne;
-    public int playerTwo;
-
-    public TennisScore(int playerOne, int playerTwo) {
-        this.playerOne = playerOne;
-        this.playerTwo = playerTwo;
-    }
-
-    public TennisScore playerOneScores() {
-        return new TennisScore(playerOne + 1, playerTwo);
-    }
-
-    public TennisScore playerTwoScores() {
-        return new TennisScore(playerOne, playerTwo + 1);
-    }
+    SimpleTennisScore playerTwoScores();
     
-    public String getDisplayString() {
-        if(playerOne == 0 && playerTwo == 0) {
-            return "Love all";
-        }
-        
-        if(playerOne == 4) {
-            return "Player 1 won";
-        }
-        
-        return scoreAsString(playerOne) + "-" + scoreAsString(playerTwo);
-    }
-
-    private String scoreAsString(int score) {
-        if(score == 0) {
-            return "Love";
-        }
-        
-        return "15";
-    }
+    String getDisplayString();
 }
