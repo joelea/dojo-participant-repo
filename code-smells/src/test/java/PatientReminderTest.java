ff71ec0d32bea614ac5f85f90ce479b612f26559
Joe Lea <jlea@palantir.com>
2014-09-24 11:31:28 +0100

    Test refactor

--- code-smells/src/test/java/PatientReminderTest.java
+++ code-smells/src/test/java/PatientReminderTest.java
@@ -11,12 +11,12 @@ public class PatientReminderTest {
     private static final String EMAIL_ADDRESS = "an@email.address";
     private final Patient emailPatient = new Patient();
     private final EmailService emailService = mock(EmailService.class);
+    private final PatientReminder reminder = new PatientReminder(emailService);
 
     @Test public void
     a_patient_with_an_email_address_receives_an_email() {
         emailPatient.setEmailAddress(EMAIL_ADDRESS);
 
-        PatientReminder reminder = new PatientReminder(emailService);
         reminder.remind(emailPatient);
         verify(emailService).emailReminderTo(EMAIL_ADDRESS);
 
