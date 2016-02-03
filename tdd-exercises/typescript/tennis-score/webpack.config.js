862586efec51cfc4456db6df2c744af0635872f6
Callum Rogers <callumr@palantir.com>
2016-02-02 21:23:12 -0500

    Add tennis-score

---
+++ tdd-exercises/typescript/tennis-score/webpack.config.js
@@ -0,0 +1,40 @@
+var child_process = require('child_process');
+var _ = require('lodash');
+
+var outputDir = __dirname + '/build';
+var outputFilename = "tests.js";
+var outputPath = outputDir + '/' + outputFilename;
+
+var mochaPath = __dirname + "/node_modules/mocha/bin/mocha";
+
+function WebpackAfterEmitPlugin(callback) {
+  this.callback = callback;
+}
+
+WebpackAfterEmitPlugin.prototype.apply = function(compiler) {
+  compiler.plugin('done', this.callback);
+};
+
+module.exports = {
+  entry: './src/tennis-score-spec.ts',
+  output: {
+    path: outputDir,
+    filename: outputFilename
+  },
+  plugins: [
+    new WebpackAfterEmitPlugin(function() {
+      _.defer(function() {
+        try {
+          child_process.execSync(mochaPath + ' ' + outputPath, {stdio: 'inherit'})
+        } catch (e) {
+
+        }
+      });
+    })
+  ],
+  module: {
+    loaders: [
+      {test: /\.ts$/, loader: 'ts-loader'}
+    ]
+  }
+};
