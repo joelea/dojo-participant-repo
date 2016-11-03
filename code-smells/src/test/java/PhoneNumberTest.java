6ebaf77b2940e79f0c3432cdd4a79121c22bdacf
Greg O'Connor <greg@gloconnor.com>
2016-11-03 16:14:40 -0700

    Add test for isMobileNumber()

--- code-smells/src/test/java/PhoneNumberTest.java
+++ code-smells/src/test/java/PhoneNumberTest.java
@@ -1,3 +1,6 @@
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 import org.junit.Test;
 
 public class PhoneNumberTest {
@@ -10,5 +13,17 @@ public class PhoneNumberTest {
     @Test(expected = IllegalArgumentException.class)
     public void fails_to_create_if_length_isnt_11() {
         new PhoneNumber("123456789");
+    }
+
+    @Test
+    public void is_mobile_number_if_begins_with_07() {
+        final PhoneNumber phoneNumber = new PhoneNumber("07123456789");
+        assertThat(phoneNumber.isMobileNumber(), is(true));
+    }
+
+    @Test
+    public void is_not_mobile_number_if_it_does_not_begin_with_07() {
+        final PhoneNumber phoneNumber = new PhoneNumber("00123456789");
+        assertThat(phoneNumber.isMobileNumber(), is(false));
     }
 }
