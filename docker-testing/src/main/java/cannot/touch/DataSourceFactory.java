50e5e280ecfc8f206f4e13d344b986b0997cff73
Joe Lea <joeleapublic@gmail.com>
2016-11-05 07:18:35 -0700

    Set a random data source name

--- docker-testing/src/main/java/cannot/touch/DataSourceFactory.java
+++ docker-testing/src/main/java/cannot/touch/DataSourceFactory.java
@@ -3,13 +3,14 @@ package cannot.touch;
 import org.postgresql.jdbc3.Jdbc3PoolingDataSource;
 
 import javax.sql.DataSource;
+import java.util.UUID;
 
 public class DataSourceFactory {
 
     public static DataSource create() {
         Jdbc3PoolingDataSource source = new Jdbc3PoolingDataSource();
 
-        source.setDataSourceName("A Data Source");
+        source.setDataSourceName(UUID.randomUUID().toString());
         source.setServerName("localhost");
         source.setDatabaseName("postgres");
         source.setPortNumber(5432);
